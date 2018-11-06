953a00c0e360910e0deeafd85d85e357b5e79eff
Andres Felipe Ardila <felipeardila1010@gmail.com>
2018-11-05 21:56:00 -0500

    Clase 10 - Formulario

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Grid, FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
+
+
+export class App extends Component {
+  state = {
+    nombre: this.props.nombre
+  };
+
+  alOprimirBoton = e => {
+    const nombre = e.target.nombre.value;
+    e.preventDefault();
+    if (typeof nombre === "string" && nombre.length > 0) {
+      this.setState({
+        nombre: e.target.nombre.value
+      });
+      e.target.nombre.value = "";
+    }
+  };
+
+  render() {
+    const { nombre } = this.state;
+    const { mensaje } = this.props;
+    return (
+      <Grid>
+        <h1>Hola {nombre}!</h1>
+        <p>{mensaje}</p>
+        <form onSubmit={this.alOprimirBoton}>
+          <FormGroup>
+            <ControlLabel>Nombre</ControlLabel>
+            <FormControl type="text" id="nombre" name="nombre"/>
+            <Button type="submit">Saludar</Button>
+          </FormGroup>
+        </form>
+      </Grid>
+    );
+  }
+}
+
+App.defaultProps = {
+  nombre: "Felipe",
+  mensaje: "Mensaje default"
+};
+
+export default App;
